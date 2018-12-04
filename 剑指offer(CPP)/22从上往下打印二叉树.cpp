fb3dbd93da08d1b7c709165c59a39d0ad790e469
charlie <fangchao1086@163.com>
2018-12-04 15:53:18 +0800

    2018/12/04

---
+++ 剑指offer(CPP)/22从上往下打印二叉树.cpp
@@ -0,0 +1,30 @@
+//从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    vector<int> PrintFromTopToBottom(TreeNode* root) {
+        queue<TreeNode *> q;
+        vector<int> r;
+        q.push(root);
+        while(!q.empty()){
+            root=q.front();
+            q.pop();
+            if(!root)
+                continue;
+            r.push_back(root->val);
+            q.push(root->left);
+            q.push(root->right);
+        }
+        return r;
+    }
+};
---
+++ 剑指offer(CPP)/23二叉搜索树的后序遍历序列.cpp
@@ -0,0 +1,32 @@
+//输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+//如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+
+
+class Solution {
+public:
+    bool VerifySquenceOfBST(vector<int> sequence) {
+        return bst(sequence, 0, sequence.size()-1);
+    }
+    
+    bool bst(vector<int> sequence, int begin, int end){
+        if(sequence.empty() || begin>end)
+            return false;
+        //根节点
+        int root = sequence[end];
+        //找到第一个右子树的节点
+        int i = begin;
+        for(;i<end;i++)
+            if(sequence[i]>root)
+                break;
+        for(int j=i;j<end;j++)
+            if(sequence[j]<root)
+                return false;
+        bool left = true;
+        bool right = true;
+        if(i>begin)
+            left = bst(sequence, begin, i-1);
+        if(i<end)
+            right = bst(sequence, i, end-1);
+        return left && right;
+    }
+};
